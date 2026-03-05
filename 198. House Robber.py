0e92d1a0ff754b1442d55fb3b53d23ed18efb7d9
JamieLandolt <jamielandolt@outlook.com>
2026-03-05 12:44:39 +1000

    198. House Robber

---
+++ 198. House Robber.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        cache = {}
+        return self.robPerm(nums, 0, cache)
+
+    def robPerm(self, nums, i, cache):
+        if i >= len(nums):
+            return 0
+
+        cur = cache.get(i + 1, None)
+        if cur is None:
+            cur = self.robPerm(nums, i + 1, cache)
+            cache[i + 1] = cur
+        nex = cache.get(i + 2, None)
+        if nex is None:
+            nex = self.robPerm(nums, i + 2, cache)
+            cache[i + 2] = nex
+
+        return max(nums[i] + nex, cur)
+
