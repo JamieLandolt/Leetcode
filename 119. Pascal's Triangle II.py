e436736d6120b9c9a80bedf0dc526962b456b014
JamieLandolt <jamielandolt@outlook.com>
2026-03-05 11:38:13 +1000

    119. Pascal's Triangle II

---
+++ 119. Pascal's Triangle II.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        return [1] + self.sums(self.getRow(rowIndex - 1)) + [1]
+
+    def sums(self, row: List[int]) -> List[int]:
+        sums = []
+        for i in range(len(row) - 1):
+            sums.append(row[i] + row[i + 1])
+        return sums
+
