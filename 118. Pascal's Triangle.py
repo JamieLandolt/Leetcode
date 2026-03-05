101f2c7812445d79884dc5cb4ebd65eeabe0d4af
JamieLandolt <jamielandolt@outlook.com>
2026-03-05 11:35:05 +1000

    118. Pascal's Triangle

---
+++ 118. Pascal's Triangle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 1:
+            return [[1]]
+        prev = self.generate(numRows - 1)
+        return prev + [[1] + self.sums(prev[-1]) + [1]]
+
+    def sums(self, row: List[int]) -> List[int]:
+        sums = []
+        for i in range(len(row) - 1):
+            sums.append(row[i] + row[i + 1])
+        return sums
