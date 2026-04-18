9656346dcbb803b91d61ec8c26bc11f8e6ea4a72
JamieLandolt <jamielandolt@outlook.com>
2026-04-18 23:19:05 +1000

    Fixed errors in Meeting Rooms II

--- Meeting Rooms II.py
+++ Meeting Rooms II.py
@@ -1,10 +1,10 @@
-"""
-Definition of Interval:
+from typing import List
+
 class Interval(object):
     def __init__(self, start, end):
         self.start = start
         self.end = end
-"""
+
 
 class Solution:
     def minMeetingRooms(self, intervals: List[Interval]) -> int:
