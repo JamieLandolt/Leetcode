e7e4ce4f6c9a3ff391b8cc24fb37573ee8bfcf59
Jamie Landolt <17292835+jamie-landolt@user.noreply.gitee.com>
2026-08-11 16:26:38 +1000

    Ammended 238

--- problems/238. Product of Array Except Self.cpp
+++ problems/238. Product of Array Except Self.cpp
@@ -4,13 +4,6 @@ public:
         vector<int> pre(nums.size(), 1);
         vector<int> post(nums.size(), 1);
 
-        if (nums.size() == 0) {
-            return nums;
-        }
-        if (nums.size() == 1) {
-            return vector<int>{1};
-        }
-
         for (int i = 1; i < nums.size(); i++) {
             pre[i] = pre[i - 1] * nums[i - 1];
         }
@@ -19,16 +12,10 @@ public:
             post[i] = post[i + 1] * nums[i + 1];
         }
 
-        for (int i = 0; i < pre.size(); i++) {
-            cout << pre[i] << "|" << post[i] << "\n";
-        }
-        
         vector<int> ans;
-        ans.push_back(post[0]);
-        for (int i = 1; i < nums.size() - 1; i++) {
+        for (int i = 0; i < nums.size(); i++) {
             ans.push_back(pre[i] * post[i]);
         }
-        ans.push_back(pre[nums.size() - 1]);
         return ans;
     }
 };
