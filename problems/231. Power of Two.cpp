dd5b91551aa1666115ad8bbe2cdfa5d7fe83afda
Jamie Landolt <17292835+jamie-landolt@user.noreply.gitee.com>
2026-08-11 15:40:08 +1000

    231

---
+++ problems/231. Power of Two.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 1) {
+            return true;
+        } else if (n & 1 || n == 0) {
+            return false;
+        }
+        return isPowerOfTwo(n >> 1);
+    }
+};
